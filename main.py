4dd3d156b9a9bbdd8bfdfc434b0c25912ba2566d
BeCyber/Pal <75971101+pal-development@users.noreply.github.com>
2021-12-29 14:29:48 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 
+file_format = "pal_face_generation-%s.jpg"
+
 amount = int(input("How many faces do you want to generate?: "))
 for count in range(amount):
     img_data = requests.get("https://thispersondoesnotexist.com/image").content
-    with open(f'pal_face_generation-{str(count)}.jpg', 'wb') as handler:
+    with open(file_format % (str(count)), 'wb') as handler:
         handler.write(img_data)
     print(f"Generated {str(count)} Faces!")
     count = count + 1
